d7a6c9c95d7d97114a8387512b34e02d96a2ebc2
Meike <mlorenz@stud.hs-bremen.de>
2021-07-08 19:44:44 +0200

    alles mögliche in der GUI
    ist noch nicht wichtig, probiere noch

--- src/main/java/com/mygroupname/StartbildGUI.java
+++ src/main/java/com/mygroupname/StartbildGUI.java
@@ -1,3 +1,5 @@
+package com.mygroupname;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -28,35 +30,30 @@ import javax.swing.ImageIcon;
  * @author Meike
  */
 public class StartbildGUI extends JFrame {       //JFrame ist die Oberklasse bei super()
-    private Image img;
+//    private Image img;
     
     public StartbildGUI(){      //Unterklasse
         super("Startbild");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        JLabel label = new JLabel("Wer wird Bionik-Bachelor? :-)");
-//        add(label);
+        JLabel label = new JLabel("Wer wird Bionik-Bachelor? :-)");
+        add(label);
         setSize(800,500);
         setLocationRelativeTo(null);
         
-//        setLayout(new BorderLayout()); 
-//        add(new JLabel("leer"), BorderLayout.NORTH);
-//        add(new JLabel ("Osten"), BorderLayout.EAST);
-//        add(new JLabel("Grofa ist nur einmal im Jahr!! B)"), BorderLayout.WEST);
+        setLayout(new BorderLayout()); 
+        add(new JLabel("leer"), BorderLayout.NORTH);
+        add(new JLabel ("Osten"), BorderLayout.EAST);
+        add(new JLabel("Grofa ist nur einmal im Jahr!! B)"), BorderLayout.WEST);
             
-        BackgroundPanel backgroundPanel = new BackgroundPanel();
-        add(backgroundPanel);
-	setSize(backgroundPanel.getBackgroundImage().getWidth(backgroundPanel),	backgroundPanel.getBackgroundImage().getHeight(backgroundPanel));
+//        BackgroundPanel backgroundPanel = new BackgroundPanel();
+//        add(backgroundPanel);
+//	setSize(backgroundPanel.getBackgroundImage().getWidth(backgroundPanel),	backgroundPanel.getBackgroundImage().getHeight(backgroundPanel));
         
         setVisible(true);
     }
 	
 
-        
-//        menütitel = "Botanik oder Chemie?";
-//        JPanel südpanel = new JPanel();
-//        PopupMenu fächer = new PopupMenu(menütitel);
-//        südpanel.add(fächer);
-//        
+             
        
         
     public static void main(String[] args) {        //MainKlasse
