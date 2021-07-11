2f660563bbf56dff7ac4073039c48428c11be65c
Arjun <Arjun.buesing@googlemail.com>
2021-07-11 16:31:33 +0200

    Fixing mainClassnotFound Exception
    still throws Nullpointer

--- src/main/java/Lernprogramm/GUI/StartbildGUI.java
+++ src/main/java/Lernprogramm/GUI/StartbildGUI.java
@@ -5,9 +5,7 @@ package Lernprogramm.GUI;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 //irgendwelche hilfreichen Packages von Java
-
 import java.awt.BorderLayout;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
@@ -17,53 +15,45 @@ import javax.swing.JFrame; //Top-Level-Container = unser Fenster
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
-
 /**
  *
  * @author Meike
  */
 public class StartbildGUI extends JFrame {
-    
+
     JPanel control = new JPanel(new BorderLayout());
     JLabel titel = new JLabel("Wer wird Bionik-Bachelor?");
     JButton logo = new JButton("Start");
     BackgroundPanel backgroundPanel = new BackgroundPanel();
-    
-public StartbildGUI(){      //Unterklasse
+
+    public StartbildGUI() {      //Unterklasse
         super("Startbild");
         setSize(800, 400);
         setLayout(new BorderLayout());
-        
+
         add(control, BorderLayout.NORTH);
         control.add(titel, BorderLayout.NORTH);
         add(logo, BorderLayout.CENTER);
-        logo.addActionListener(new ActionListener(){
+        logo.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 showFrame();
             }
 
-    private void showFrame() {
-                Fragenkarte();
+            private void showFrame() {
+                //Fragenkarte();
             }
 
-    
         });
-               
+
         add(backgroundPanel, BorderLayout.SOUTH);
         setSize(backgroundPanel.getBackgroundImage().getWidth(backgroundPanel), backgroundPanel.getBackgroundImage().getHeight(backgroundPanel));
-        
-        
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
     }
-    
 
-
-public static void main(String[] args) { 
-        new StartbildGUI();
+    public static void main(String[] args) {
+       var gui = new StartbildGUI();
+    }
 }
-}
-
-        
