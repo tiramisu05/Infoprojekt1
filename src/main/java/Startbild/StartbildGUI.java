61e3f76599525fc87a9c1f3e34d0845f560fd142
Meike <mlorenz@stud.hs-bremen.de>
2021-07-12 14:09:04 +0200

    StartbildGUI

--- src/main/java/Startbild/StartbildGUI.java
+++ src/main/java/Startbild/StartbildGUI.java
@@ -5,9 +5,12 @@ package Startbild;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 //irgendwelche hilfreichen Packages von Java
+
+import Lernprogramm.Logik.Programm;
 import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.Font;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,67 +18,49 @@ import javax.swing.JButton;
 import javax.swing.JFrame; //Top-Level-Container = unser Fenster
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.RootPaneContainer;
+import javax.swing.SwingConstants;
+
+
 
 /**
  *
  * @author Meike
  */
 public class StartbildGUI extends JFrame {
-
+    
     JPanel control = new JPanel(new BorderLayout());
-    JLabel titel = new JLabel("Wer wird Bionik-Bachelor?");
-    JButton start = new JButton("Start");
+    JLabel titel = new JLabel("Wer wird Bionik-Bachelor?", SwingConstants.CENTER);
+    JButton startbut = new JButton("Start");
     BackgroundPanel backgroundPanel = new BackgroundPanel();
-
-    public StartbildGUI() {      //Unterklasse
+    
+public StartbildGUI(){      //Unterklasse
         super("Startbild");
-        setSize(500, 250);
         setLayout(new BorderLayout());
-<<<<<<< HEAD:src/main/java/Lernprogramm/GUI/StartbildGUI.java
-
-        add(control, BorderLayout.NORTH);
-        control.add(titel, BorderLayout.NORTH);
-        add(logo, BorderLayout.CENTER);
-        logo.addActionListener(new ActionListener() {
-=======
         
         
-        add(backgroundPanel);
-        setSize(backgroundPanel.getBackgroundImage().getWidth(backgroundPanel), backgroundPanel.getBackgroundImage().getHeight(backgroundPanel));
+        getContentPane().add(backgroundPanel);
+        pack();
+        setSize(backgroundPanel.getBackgroundImage().getWidth(backgroundPanel), backgroundPanel.getBackgroundImage().getWidth(backgroundPanel));
+        titel.setFont(new Font( "Times New Roman", Font.BOLD, 26));
         add(titel, BorderLayout.NORTH);
-        add(start, BorderLayout.SOUTH);
+        add(startbut, BorderLayout.SOUTH);
         
-        logo.addActionListener(new ActionListener(){
->>>>>>> MeikesStand:src/main/java/Startbild/StartbildGUI.java
+        startbut.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
-                showFrame();
+                new Programm();
+                dispose();
             }
+            });     
 
-<<<<<<< HEAD:src/main/java/Lernprogramm/GUI/StartbildGUI.java
-            private void showFrame() {
-                //Fragenkarte();
-=======
-    private void showProgram() {
-               
->>>>>>> MeikesStand:src/main/java/Startbild/StartbildGUI.java
-            }
-
-        });
-<<<<<<< HEAD:src/main/java/Lernprogramm/GUI/StartbildGUI.java
-
-        add(backgroundPanel, BorderLayout.SOUTH);
-        setSize(backgroundPanel.getBackgroundImage().getWidth(backgroundPanel), backgroundPanel.getBackgroundImage().getHeight(backgroundPanel));
-=======
        
->>>>>>> MeikesStand:src/main/java/Startbild/StartbildGUI.java
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
+        
     }
 
     public static void main(String[] args) {
-       var gui = new StartbildGUI();
+        new StartbildGUI();
     }
+
 }
